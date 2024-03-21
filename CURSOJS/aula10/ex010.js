c8839ae6e33249482ed46e41409f62a12b6f39e6
Italo_Leonardo <italoleonardo69@gmail.com>
2024-03-21 06:08:51 -0300

    add JS

---
+++ CURSOJS/aula10/ex010.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log(`A velocidade do seu carro é ${vel}km/k`)
+if (vel > 60) {
+    console.log(`Você ultrapasou a velocidade permitida> MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
