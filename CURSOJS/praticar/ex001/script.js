c47ebec73834b1f2b3255ad24496bbca0e09e6c9
Italo_Leonardo <italoleonardo69@gmail.com>
2024-03-30 20:27:46 -0300

    add html, css e js

--- CURSOJS/praticar/ex001/script.js
+++ CURSOJS/praticar/ex001/script.js
@@ -1,1 +1,24 @@
-window.alert('Ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 8
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >=0 && hora < 12) {
+        // BOM DIA
+        img.src = "css/img/manhaWed.png"
+        document.body.style.background = "#e2cd9f"
+    } else if(hora >= 12 && hora < 18){
+        // BOA TARDE
+        img.src = "css/img/tardeWed.png"
+        document.body.style.background = "#b9846f"
+    } else {
+        // BOA NOITE
+        img.src = "css/img/noiteWed.png"
+        document.body.style.background = "#515154"
+    }
+}
+
